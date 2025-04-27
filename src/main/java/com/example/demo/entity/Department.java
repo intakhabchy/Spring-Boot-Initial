39f3b5acfc6a54bb3f01913eefb21ded1b0b9a66
intakhabchy <intakhab.chy@gmail.com>
2025-04-28 00:55:05 +0600

    CRUD - with naming convention

---
+++ src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long depaartmentId;
+    public String departmentName;
+    public String departmentAddress;
+    public String departmentCode;
+
+    public Long getDepaartmentId() {
+        return depaartmentId;
+    }
+
+    public void setDepaartmentId(Long depaartmentId) {
+        this.depaartmentId = depaartmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    public Department(Long depaartmentId, String departmentName, String departmentAddress, String departmentCode) {
+        this.depaartmentId = depaartmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+        this.departmentCode = departmentCode;
+    }
+
+    public Department() {
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "depaartmentId=" + depaartmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", departmentAddress='" + departmentAddress + '\'' +
+                ", departmentCode='" + departmentCode + '\'' +
+                '}';
+    }
+}
